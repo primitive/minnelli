0e748192e152a9291df7f4d71f7f496322b53035
Shaun Knowles <sknow@skmbp1.home>
2020-03-17 14:18:44 +0000

    initial commit

---
+++ build/precache-manifest.2723d6e33c9e5b02b1f220247d079f3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c3df4bd6a9a0fb5a8b4ca90c50ff9b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95ba07b164d92d5b3bc6",
+    "url": "./static/css/main.49b44338.chunk.css"
+  },
+  {
+    "revision": "24d16decd6e199b11db7",
+    "url": "./static/js/2.d51db097.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.d51db097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ba07b164d92d5b3bc6",
+    "url": "./static/js/main.daf1256f.chunk.js"
+  },
+  {
+    "revision": "a87c49cfa27f7da5c5fd",
+    "url": "./static/js/runtime-main.11d0f188.js"
+  },
+  {
+    "revision": "2d968a9a61160ffbdee78dc0c95d38f1",
+    "url": "./static/media/primitive.2d968a9a.svg"
+  },
+  {
+    "revision": "c4108f5323aef251fa65cb4d9e04b3ca",
+    "url": "./static/media/punkyicon.c4108f53.svg"
+  }
+]);
